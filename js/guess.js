941b4bf757848a49052757db3e61353d888a781a
Domenic Corso <domenic.corso@live.com>
2017-06-21 21:36:54 +1000

    Added more function bodies to Guess

--- js/guess.js
+++ js/guess.js
@@ -3,10 +3,11 @@ function Guess(uiCallback) {
     
     this.word = "";
     this.guessableLetters = 0;
+    this.revealedLetters = 0;
     this.hintsRemaining = 0;
     
-    this.incorrectGuesses = [];
-    this.correctGuesses = [];
+    this.incorrectLetters = [];
+    this.correctLetters = [];
     
     this.guessed = false;
 }
@@ -15,4 +16,35 @@ Guess.prototype.setWord = function(word, hints) {
     this.word = word;
     this.hintsRemaining = hints;
     this.uiCallback.updateGuess(this);
+};
+
+/*
+    This function should be called when the user selects a single letter
+    to guess. The letter passed in should either end up in 'incorrectLetters'
+    or 'correctLetters'.
+    
+    If the word contains multiple of the same letter, they both should be 
+    revealed however only one should be added to the 'correctLetters' array.
+*/
+Guess.prototype.tryLetter = function(letter) {
+    
+};
+
+/*
+    This should be called at the end of 'tryLetter()', it checks if the player
+    has guessed all the letters in the word.
+    
+    A player has guessed all the letters if 'guessableLetters' is equal to 
+    'revealedLetters'.
+*/
+Guess.prototype.checkForCompletion = function() {
+    
+};
+
+/*
+    A random letter from the word should be picked and then passed to the 
+    'tryLetter()' method which will handle the rest.
+*/
+Guess.prototype.hint = function() {
+    
 };
